a1542d857b2259eb38c08eaf2efe4c932a2c9079
zohaib <apricopt@gmail.com>
2020-12-25 17:38:27 +0500

    working axios with strapi on http

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+function subwalamit(e) {
+  e.preventDefault();
+  toastr.options = {
+    closeButton: false,
+    debug: false,
+    newestOnTop: true,
+    progressBar: true,
+    positionClass: "toast-top-full-width",
+    preventDuplicates: false,
+    showDuration: "90000",
+    hideDuration: "1000",
+    timeOut: "5000",
+    extendedTimeOut: "1000",
+    showEasing: "swing",
+    hideEasing: "linear",
+    showMethod: "fadeIn",
+    hideMethod: "fadeOut",
+  };
+  console.log("yeah called");
+  console.log(e.target.email);
+
+  const data = {
+    name: e.target.name.value,
+    email: e.target.email.value,
+    phone: e.target.phone.value,
+  };
+
+  axios
+    .post("http://toxicmodelmgmt.com:5002/mentoriademodelos", data)
+    .then((result) => {
+      console.log("data has been submitted succesfully", result);
+      document.getElementById("myModal").style.background = "transparent";
+      toastr["info"]("Você foi inscrito com sucesso");
+    })
+    .catch((err) => console.log(err));
+}
